3a1b77536b14864e39785f09e691b53359edd93b
ckersss <515403367@qq.com>
2022-05-12 16:54:25 +0800

    1.0

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<view class="">
+			首页
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
